579285d16e16cf00cf4f58625ba0870b7776e5c4
santoslopeskemilly-code <santos.lopes.kemilly@escola.pr.gov.br>
2026-09-18 13:53:07 -0300

    insercao_afirmacoes

--- script.js
+++ script.js
@@ -10,7 +10,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Isso é assustador!",
-                afirmacao: "afirmacao"
+                afirmacao:
+                "No início ficou com medo do que essa tecnologia pode fazer. ",
+          "Achou assustador pensar na velocidade na qual a tecnologia está avançando.",
+        ],
+
             },
             {
                 texto: "Isso é maravilhoso!",
